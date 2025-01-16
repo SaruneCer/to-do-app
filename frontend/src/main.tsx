50d111a54eb92730fa00a6a189002f4f5f95857e
s.cernikeviciene <sarune.ku@gmail.com>
2025-01-17 01:24:50 +0200

    Set up main entry file with router and routes configuration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import { ROUTES } from "./router/pageRoutes.js";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserInfo from "./pages/UserInfo";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: ROUTES.HOME,
+        element: <Home />,
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <Login />,
+      },
+      {
+        path: ROUTES.USER_INFO,
+        element: <UserInfo />,
+      },
+      {
+        path: ROUTES.REGISTER,
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
