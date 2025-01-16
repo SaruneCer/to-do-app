c224f4693d2a21feb114e09d993cc669342c4067
s.cernikeviciene <sarune.ku@gmail.com>
2025-01-17 01:25:20 +0200

    Set up App component with Topbar, Footer, and UserProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import Topbar from "./components/Topbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import { UserProvider } from "./context/UserContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <Topbar />
+          <Outlet />
+          <Footer />
+        </UserProvider>
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
