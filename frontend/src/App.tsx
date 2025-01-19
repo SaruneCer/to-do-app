6f47c42055fad456b7664d6eb708dd36af0931c3
s.cernikeviciene <sarune.ku@gmail.com>
2025-01-19 11:54:59 +0200

    Move router configuration from main.tsx to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,71 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./components/layout/Footer";
-import { UserProvider } from "./context/UserContext";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserInfo from "./pages/UserInfo";
+import Error from "./pages/Error";
+import AuthLayout from "./components/layout/AuthLayout";
+import RootLayout from "./components/layout/RootLayout";
+import { UserProvider, UserContext } from "./context/UserContext";
+import { ROUTES } from "./router/pageRoutes";
+import "./styles/global.scss";
+import { useContext } from "react";
 
 const queryClient = new QueryClient();
 
-function App() {
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+  const { isLoggedIn } = useContext(UserContext);
+  if (!isLoggedIn) {
+    return <Navigate to={ROUTES.LOGIN} />;
+  }
+  return element;
+};
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: ROUTES.HOME,
+        element: <ProtectedRoute element={<Home />} />,
+      },
+      {
+        path: ROUTES.USER_INFO,
+        element: <ProtectedRoute element={<UserInfo />} />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: ROUTES.LOGIN,
+        element: <Login />,
+      },
+      {
+        path: ROUTES.REGISTER,
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
   return (
     <div>
       <QueryClientProvider client={queryClient}>
         <UserProvider>
-          <Outlet />
-          <Footer />
+          <RouterProvider router={router} />
         </UserProvider>
       </QueryClientProvider>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,54 +1,67 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import UserInfo from "./pages/UserInfo";
+// import Error from "./pages/Error";
+// import App from "./App";  
+// import AuthLayout from "./components/layout/AuthLayout";
+// import RootLayout from "./components/layout/RootLayout";
+// import "./styles/global.scss";
+// import { ROUTES } from "./router/pageRoutes"; 
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         element: <RootLayout />,
+//         children: [
+//           {
+//             path: ROUTES.HOME,
+//             element: <Home />,
+//           },
+//           {
+//             path: ROUTES.USER_INFO,
+//             element: <UserInfo />,
+//           },
+//         ],
+//       },
+//       {
+//         element: <AuthLayout />,
+//         children: [
+//           {
+//             path: ROUTES.LOGIN,
+//             element: <Login />,
+//           },
+//           {
+//             path: ROUTES.REGISTER,
+//             element: <Register />,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import UserInfo from "./pages/UserInfo";
-import Error from "./pages/Error";
-import App from "./App.jsx";
-import AuthLayout from "./components/layout/AuthLayout";
-import RootLayout from "./components/layout/RootLayout";
-import "./styles/global.scss";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        element: <RootLayout />, 
-        children: [
-          {
-            path: "/",
-            element: <Home />,
-          },
-          {
-            path: "/user-info",
-            element: <UserInfo />,
-          },
-        ],
-      },
-      {
-        element: <AuthLayout />, 
-        children: [
-          {
-            path: "/login",
-            element: <Login />,
-          },
-          {
-            path: "/register",
-            element: <Register />,
-          },
-        ],
-      },
-    ],
-  },
-]);
+import App from "./App";
+// import "./styles/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
