6cb251b34f0f3722b332acaedfc49f8ed7ceecbe
s.cernikeviciene <sarune.ku@gmail.com>
2025-01-19 15:30:46 +0200

    Update Button component to have confirm and cancel styles

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -4,13 +4,17 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   rounded?: boolean;
   small?: boolean;
   large?: boolean;
+  confirm?: boolean;  
+  cancel?: boolean;   
 }
 
-const Button = ({ className = "", small, large, ...props }: ButtonProps) => {
+const Button = ({ className = "", small, large, confirm, cancel, ...props }: ButtonProps) => {
   const buttonClassName = `
     ${styles.button}
     ${small ? styles.small : ""}
     ${large ? styles.large : ""}
+    ${confirm ? styles.confirm : ""}
+    ${cancel ? styles.cancel : ""}
     ${className}
   `
     .trim()
